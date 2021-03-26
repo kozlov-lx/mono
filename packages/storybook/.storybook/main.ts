31963ec2aaa7b06822c8a2fdaeac21b8666b4b44
Alexey A. Kozlov <kozlov.lx@gmail.com>
2021-03-26 21:36:36 +0400

    feat: init storybook

---
+++ packages/storybook/.storybook/main.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+const stories = [
+  '../src/**/*.stories.mdx',
+  '../src/**/*.stories.@(js|jsx|ts|tsx)',
+];
+
+const addons = [
+  '@storybook/addon-links',
+  '@storybook/addon-essentials',
+];
+
+module.exports = {
+  stories,
+  addons,
+};
